8977db88f4f39a76ff7fdf015b98d3303e9f42bb
kotpavlik <kotpavlik88@gmail.com>
2022-03-30 23:55:22 +0300

    homework-1

--- src/p2-homeworks/h1/HW1.tsx
+++ src/p2-homeworks/h1/HW1.tsx
@@ -4,8 +4,8 @@ import Message from "./Message";
 
 const messageData = {
     avatar: 'https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg',
-    name: 'Some Name',
-    message: 'some text',
+    name: 'Kot Pavlik',
+    message: 'Hello Ignat !!',
     time: '22:00',
 }
 
@@ -13,9 +13,6 @@ function HW1() {
     return (
         <div>
             <hr/>
-            homeworks 1
-
-            should work (должно работать)
 
             <Message
                 avatar={messageData.avatar}
--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import s from './Message.module.css'
 
-function Message(props:any) {
+type PropsType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
+
+function Message(props: PropsType) {
     return (
-        <div>
-hello
+        <div className={s.message_wrapper}>
+            <div className={s.avatar_block}>
+                <img src={props.avatar} alt=""/>
+            </div>
+            <div className={s.message_block}>
+                <div className={s.name_message}>
+                    <div className={s.name}>{props.name}</div>
+                    <div className={s.messages}>{props.message}</div>
+                </div>
+                <div className={s.contain_for_time}>
+                    <div className={s.time}>{props.time}</div>
+                </div>
+
+            </div>
+
         </div>
     )
 }
